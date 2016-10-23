0b6c7c8c05c0464f3a49bbfc79b173941d351943
filippo-personal <gatti.flpp@gmail.com>
2016-10-23 21:09:32 +0200

    Plot b&w lfhf filter

--- syn2ann/lfhf_filter_plot.m
+++ syn2ann/lfhf_filter_plot.m
@@ -17,14 +17,27 @@ function lfhf_filter_plot(varargin)
     WLF(1:nfa) = 1.0;
     WLF(nfa+1:nfb) = 0.5*(1+cos(fac*dfr*(0:nfb-nfa-1)));
     WHF = 1-WLF;
-    color_order = [rgb('IntenseBlue');rgb('IntenseOrange');rgb('blue');rgb('red')];
+    color_order = [rgb('Black');rgb('Grey');rgb('black');rgb('black')];
     set(0,'defaultaxescolororder',color_order);
     fpplot('xpl',{vfr;vfr;[fa;fa];[fb;fb]},'ypl',{WLF;WHF;[0,1];[0,1]},...
         'pfg',[0 0 12 12],'scl',{'slx'},'xlb',{'f [Hz]'},'xlm',{[1e-1,5e1]},...
         'xtk',{[fa,fb]},'ylb',{'[1]'},'ylm',{[-.05,1.3]},...
-        'ytk',{0:.25:1},'leg',{{'$\mathbf{w^{LF}\left(f\right)}$','$\mathbf{w^{HF}\left(f\right)}$'}},...
-        'tit',{'LF-HF HYBRIDIZATION'},'lst',{'-';'-';'--';'--'});
-    set(gca,'xticklabel',{'f_{LF}';'f_{HF}'})
+        'ytk',{0:.25:1},'leg',{{'$w^{LF}\left(f\right)$','$w^{HF}\left(f\right)$'}},...
+        'tit',{'LF-HF HYBRIDIZATION'},'lst',{'-';'-';'--';'--'},'vfg','on');
+    set(gca,'xticklabel',{'$f_{LF}$';'$f_{HF}$'});
+    set(gca,'TickLabelInterpreter', 'latex');
+    
+    
+    color_order = [rgb('Blue');rgb('Red');rgb('IntenseGreen');rgb('IntenseGreen')];
+    set(0,'defaultaxescolororder',color_order);
+    fpplot('xpl',{vfr;vfr;[fa;fa];[fb;fb]},'ypl',{WLF;WHF;[0,1];[0,1]},...
+        'pfg',[0 0 12 12],'scl',{'slx'},'xlb',{'f [Hz]'},'xlm',{[1e-1,5e1]},...
+        'xtk',{[fa,fb]},'ylb',{'[1]'},'ylm',{[-.05,1.3]},...
+        'ytk',{0:.25:1},'leg',{{'$w^{LF}\left(f\right)$','$w^{HF}\left(f\right)$'}},...
+        'tit',{'LF-HF HYBRIDIZATION'},'lst',{'-';'-';'--';'--'},'vfg','on');
+    set(gca,'xticklabel',{'$f_{LF}$';'$f_{HF}$'});
+    set(gca,'TickLabelInterpreter', 'latex');
+    
     if nargin>4
         sp = varargin{5};
         saveas(gcf,sp,'epsc');
