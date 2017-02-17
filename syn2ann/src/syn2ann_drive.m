6c90574091eba3b211ba3df69fe7d7cfb907e157
Filippo Gatti - vigne <filippo.gatti@centralesupelec.fr>
2017-02-17 20:00:29 +0100

    Run maps all over the Emilia monitors

--- syn2ann/src/syn2ann_drive.m
+++ syn2ann/src/syn2ann_drive.m
@@ -8,49 +8,49 @@
 % from numerical simulations (SPEED/HISADA) to target spectra obtained via
 % Artificial Neural Networks.
 %% *N.B.*
-% By setting flag_map, the user can run a fast analysis to plot single station 
+% By setting flag_map, the user can run a fast analysis to plot single station
 % time-histories/spectra (flag_map=0, demonstrative case) or run an analysis to
-% obtain shake maps (flag_map=1, parallel implementation on remote cluster). 
+% obtain shake maps (flag_map=1, parallel implementation on remote cluster).
 % Need for:
 % _syn2ann_setup_maps.m,syn2ann_run_maps.m,
 % syn2ann_setup_fast.m,syn2ann_run_fast.m_
 
 %function syn2ann_drive(varargin)
 
-    flag_map = 1;
+flag_map = 1;
 
 %    if nargin>0
 %        flag_map = varargin{1};
 %    end
 %
-    if flag_map 
-
-        %% *1). ANALYSIS SET-UP*
-        syn2ann_setup_maps;
-
-        %% *2). RUN SYN2ANN TO GET SHAKE MAPS*
-        syn2ann_run_maps;
-
-    else
-
-        %% *1). ANALYSIS SET-UP*
-        syn2ann_setup_fast;
-
-        %% *2). PARSING REC*
-        syn2ann_rec_drive;
-
-        %% *3). RUN SYN2ANN ON SINGLE STATIONS*
-        syn2ann_run;
-
-        % %% *4). COMPUTE COHERENCY*
-        % syn2ann_coherency_drive;
-
-        % %% *5). SAVE RESULTS*
-        % syn2ann_save_res;
-
-        %% *6). PLOT RESULTS*
-        syn2ann_plot_res_single;
-    end
+if flag_map
+    
+    %% *1). ANALYSIS SET-UP*
+    syn2ann_setup_maps;
+    
+    %% *2). RUN SYN2ANN TO GET SHAKE MAPS*
+    syn2ann_run_maps;
+    
+else
+    
+    %% *1). ANALYSIS SET-UP*
+    syn2ann_setup_fast;
+    
+    %% *2). PARSING REC*
+    syn2ann_rec_drive;
+    
+    %% *3). RUN SYN2ANN ON SINGLE STATIONS*
+    syn2ann_run;
+    
+    % %% *4). COMPUTE COHERENCY*
+    % syn2ann_coherency_drive;
+    
+    % %% *5). SAVE RESULTS*
+    % syn2ann_save_res;
+    
+    %% *6). PLOT RESULTS*
+    syn2ann_plot_res_single;
+end
 %    return
 %
 %end
