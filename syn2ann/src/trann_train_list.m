57e7ac5fc64bcf8da60aa3254b4d5da0612ae7e9
Filippo <filippo.gatti@polimi.it>
2017-07-14 18:45:40 +0200

    Produce new nets

--- syn2ann/src/trann_train_list.m
+++ syn2ann/src/trann_train_list.m
@@ -1,83 +1,131 @@
 %% *ANN METADATA ann*
-ann.trn.nr = 6;
-% % gh <---> AB
-% ann.trn.mtd(1).TnC = 0.75;
-% ann.trn.mtd(1).cp = 'gh';
-% ann.trn.mtd(1).scl = 'AB';
-% % ud <---> AB
-% ann.trn.mtd(2).TnC = 0.75;
-% ann.trn.mtd(2).cp = 'ud';
-% ann.trn.mtd(2).scl = 'AB';
-% % gh <---> CD
-% ann.trn.mtd(3).TnC = 0.75;
-% ann.trn.mtd(3).cp = 'gh';
-% ann.trn.mtd(3).scl = 'CD';
-% % ud <---> CD
-% ann.trn.mtd(4).TnC = 0.75;
-% ann.trn.mtd(4).cp = 'ud';
-% ann.trn.mtd(4).scl = 'CD';
-% gh <---> ALL
+ann.trn.nr = 18;
+
+% ALL - GH - TnC = 0.50 s
 ann.trn.mtd(1).TnC = 0.50;
-ann.trn.mtd(1).cp = 'gh';
-ann.trn.mtd(1).scl = 'AB';
+ann.trn.mtd(1).cp  = 'gh';
+ann.trn.mtd(1).scl = 'ALL';
 ann.trn.mtd(1).train_strategy = 'classic';
 ann.trn.mtd(1).nhn = 30;
 
-% ud <---> AB
+% ALL - GH - TnC = 0.75 s
 ann.trn.mtd(2).TnC = 0.75;
-ann.trn.mtd(2).cp = 'gh';
-ann.trn.mtd(2).scl = 'AB';
+ann.trn.mtd(2).cp  = 'gh';
+ann.trn.mtd(2).scl = 'ALL';
 ann.trn.mtd(2).train_strategy = 'classic';
 ann.trn.mtd(2).nhn = 30;
 
-% gh <---> AB
+% ALL - GH - TnC = 1.00 s
 ann.trn.mtd(3).TnC = 1.00;
-ann.trn.mtd(3).cp = 'gh';
-ann.trn.mtd(3).scl = 'AB';
+ann.trn.mtd(3).cp  = 'gh';
+ann.trn.mtd(3).scl = 'ALL';
 ann.trn.mtd(3).train_strategy = 'classic';
 ann.trn.mtd(3).nhn = 30;
 
-% ud <---> AB
+% ALL - UD - TnC = 0.50 s
 ann.trn.mtd(4).TnC = 0.50;
-ann.trn.mtd(4).cp = 'ud';
-ann.trn.mtd(4).scl = 'AB';
+ann.trn.mtd(4).cp  = 'ud';
+ann.trn.mtd(4).scl = 'ALL';
 ann.trn.mtd(4).train_strategy = 'classic';
 ann.trn.mtd(4).nhn = 30;
 
-% ud <---> AB
+% ALL - UD - TnC = 0.75 s
 ann.trn.mtd(5).TnC = 0.75;
-ann.trn.mtd(5).cp = 'ud';
-ann.trn.mtd(5).scl = 'AB';
+ann.trn.mtd(5).cp  = 'ud';
+ann.trn.mtd(5).scl = 'ALL';
 ann.trn.mtd(5).train_strategy = 'classic';
 ann.trn.mtd(5).nhn = 30;
 
-% gh <---> AB
+% ALL - UD - TnC = 1.00 s
 ann.trn.mtd(6).TnC = 1.00;
-ann.trn.mtd(6).cp = 'ud';
-ann.trn.mtd(6).scl = 'AB';
+ann.trn.mtd(6).cp  = 'ud';
+ann.trn.mtd(6).scl = 'ALL';
 ann.trn.mtd(6).train_strategy = 'classic';
 ann.trn.mtd(6).nhn = 30;
-%
-% % ud <---> ALL
-% ann.trn.mtd(4).TnC = 0.5;
-% ann.trn.mtd(4).cp = 'ud';
-% ann.trn.mtd(4).scl = 'ALL';
-% ann.trn.mtd(4).train_strategy = 'classic';
-% ann.trn.mtd(4).nhn = 150;
-% 
-% % gh <---> ALL
-% ann.trn.mtd(5).TnC = 0.5;
-% ann.trn.mtd(5).cp = 'gh';
-% ann.trn.mtd(5).scl = 'ALL';
-% ann.trn.mtd(5).train_strategy = 'classic';
-% ann.trn.mtd(5).nhn = 180;
-% 
-% % ud <---> ALL
-% ann.trn.mtd(6).TnC = 0.5;
-% ann.trn.mtd(6).cp = 'ud';
-% ann.trn.mtd(6).scl = 'ALL';
-% ann.trn.mtd(6).train_strategy = 'classic';
-% ann.trn.mtd(6).nhn = 180;
+
+% AB - GH - TnC = 0.50 s
+ann.trn.mtd(7).TnC = 0.50;
+ann.trn.mtd(7).cp  = 'gh';
+ann.trn.mtd(7).scl = 'AB';
+ann.trn.mtd(7).train_strategy = 'classic';
+ann.trn.mtd(7).nhn = 30;
+
+% AB - GH - TnC = 0.75 s
+ann.trn.mtd(8).TnC = 0.75;
+ann.trn.mtd(8).cp  = 'gh';
+ann.trn.mtd(8).scl = 'AB';
+ann.trn.mtd(8).train_strategy = 'classic';
+ann.trn.mtd(8).nhn = 30;
+
+% AB - GH - TnC = 1.00 s
+ann.trn.mtd(9).TnC = 1.00;
+ann.trn.mtd(9).cp  = 'gh';
+ann.trn.mtd(9).scl = 'AB';
+ann.trn.mtd(9).train_strategy = 'classic';
+ann.trn.mtd(9).nhn = 30;
+
+% AB - UD - TnC = 0.50 s
+ann.trn.mtd(10).TnC = 0.50;
+ann.trn.mtd(10).cp  = 'ud';
+ann.trn.mtd(10).scl = 'AB';
+ann.trn.mtd(10).train_strategy = 'classic';
+ann.trn.mtd(10).nhn = 30;
+
+% AB - UD - TnC = 0.75 s
+ann.trn.mtd(11).TnC = 0.75;
+ann.trn.mtd(11).cp  = 'ud';
+ann.trn.mtd(11).scl = 'AB';
+ann.trn.mtd(11).train_strategy = 'classic';
+ann.trn.mtd(11).nhn = 30;
+
+% AB - UD - TnC = 1.00 s
+ann.trn.mtd(12).TnC = 1.00;
+ann.trn.mtd(12).cp  = 'ud';
+ann.trn.mtd(12).scl = 'AB';
+ann.trn.mtd(12).train_strategy = 'classic';
+ann.trn.mtd(12).nhn = 30;
+
+% CD - GH - TnC = 0.50 s
+ann.trn.mtd(13).TnC = 0.50;
+ann.trn.mtd(13).cp  = 'gh';
+ann.trn.mtd(13).scl = 'CD';
+ann.trn.mtd(13).train_strategy = 'classic';
+ann.trn.mtd(13).nhn = 30;
+
+% CD - GH - TnC = 0.75 s
+ann.trn.mtd(14).TnC = 0.75;
+ann.trn.mtd(14).cp  = 'gh';
+ann.trn.mtd(14).scl = 'CD';
+ann.trn.mtd(14).train_strategy = 'classic';
+ann.trn.mtd(14).nhn = 30;
+
+% CD - GH - TnC = 1.00 s
+ann.trn.mtd(15).TnC = 1.00;
+ann.trn.mtd(15).cp  = 'gh';
+ann.trn.mtd(15).scl = 'CD';
+ann.trn.mtd(15).train_strategy = 'classic';
+ann.trn.mtd(15).nhn = 30;
+
+% CD - UD - TnC = 0.50 s
+ann.trn.mtd(16).TnC = 0.50;
+ann.trn.mtd(16).cp  = 'ud';
+ann.trn.mtd(16).scl = 'CD';
+ann.trn.mtd(16).train_strategy = 'classic';
+ann.trn.mtd(16).nhn = 30;
+
+% CD - UD - TnC = 0.75 s
+ann.trn.mtd(17).TnC = 0.75;
+ann.trn.mtd(17).cp  = 'ud';
+ann.trn.mtd(17).scl = 'CD';
+ann.trn.mtd(17).train_strategy = 'classic';
+ann.trn.mtd(17).nhn = 30;
+
+% CD - UD - TnC = 1.00 s
+ann.trn.mtd(18).TnC = 1.00;
+ann.trn.mtd(18).cp  = 'ud';
+ann.trn.mtd(18).scl = 'CD';
+ann.trn.mtd(18).train_strategy = 'classic';
+ann.trn.mtd(18).nhn = 30;
 
 % _database_
 for i_ = 1:ann.trn.nr
