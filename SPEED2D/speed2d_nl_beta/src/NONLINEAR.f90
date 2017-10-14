aaafdfb2a9a72b8889061a16895af5dced6317ea
Filippo <filippo.gatti@centralesupelec.fr>
2017-10-14 18:08:06 +0200

    Check nonlinear integrator

--- SPEED2D/speed2d_nl_beta/src/NONLINEAR.f90
+++ SPEED2D/speed2d_nl_beta/src/NONLINEAR.f90
@@ -652,6 +652,7 @@ module nonlinear2d
             integer*4                           :: counter,k,j
             real*8, parameter :: FTOL_DRIFT =   0.000001D0 
             ! INITIAL PLASTIC CONDITION
+            call mises_yld_locus(stress,center,radius,syld,F0,gradF0)
             call stiff_matrix(lambda,mu,DEL)
             ! ***** CRITICAL STATE EXTENSION *****
             ! call STIFF_MATRIX_CRITICAL(stress0,dincrement,lambda,mu,DEL)
@@ -666,7 +667,6 @@ module nonlinear2d
                 hard = hard - kkin*dot_product(gradF0,center)
                 ! COMPUTE BETA FOR DRIFT CORRECTION
                 beta  = zero
-                tempv = matmul(MM1,gradF0)
                 tempv = matmul(DEL,tempv)
                 beta  = dot_product(tempv,gradF0)
                 beta = F0/(hard+beta)
@@ -676,7 +676,6 @@ module nonlinear2d
                 call update_stress(stress,stresst,dstress)
                 centert = center+beta*((two*PHI*ckin/three)*matmul(MM1,gradF0)-center*kkin)
                 radiust = radius+beta*(Rinf-radius)*biso
-                pstraint = pstrain+beta*matmul(MM1,gradF0)
                 ! CHECK DRIFT
                 call mises_yld_locus(stresst,centert,radiust,syld,F1,gradF1)
                 if (abs(F1).gt.abs(F0)) then
@@ -692,7 +691,7 @@ module nonlinear2d
                 stress = stresst
                 center = centert
                 radius = radiust
-                pstrain = pstraint 
+                pstrain = pstrain+beta*gradF0
                 
                 if (abs(F1).le.FTOL_DRIFT) then
                     exit
@@ -828,6 +827,68 @@ module nonlinear2d
             return
             !
         end subroutine gotoFpegasus
+        !
+        subroutine gotoFtan(start0,dtrial0,F0,Ftrial,center,radius,s0,alpha)
+            real(fpp), dimension(0:5), intent(in)    :: start0,dtrial0,center
+            real(fpp),                 intent(in)    :: radius,s0
+            real(fpp),                 intent(inout) :: F0,Ftrial
+            real(fpp),                 intent(out)   :: alpha
+            real(fpp), dimension(0:5)                :: start,gradF,dev,dev_temp,temp,dev0
+            real(fpp)                                :: Fstart,err0,err1,beta
+            integer                             :: counter
+            call tensor_deviator(start0,dev0)
+            alpha=F0/(F0-Ftrial)
+            start(0:5)=start0(0:5)
+            temp=start+alpha*dtrial0
+            do counter=0,9
+                call tensor_deviator(temp, dev_temp)
+                call tensor_deviator(start,dev)
+                call tau_mises(-dev+dev_temp,err0)
+                call tau_mises(dev-dev0,err1)
+                err0=err0/err1
+                start(0:5)=temp(0:5)
+                call mises_yld_locus(start,center,radius,s0,Fstart,gradF)
+                if (abs(Fstart).le.FTOL .or. err0.lt.0.000001) then
+                    exit
+                end if
+                beta=sum(gradF(0:5)*dtrial0(0:5))
+                alpha=alpha-Fstart/beta
+                temp(0:5)=start(0:5)-(Fstart/beta)*dtrial0(0:5)
+            end do
+        end subroutine gotoFtan
+
+        subroutine gotoFsec(start0,dtrial0,F0,Ftrial,center,radius,s0,alpha)
+            real(fpp), dimension(0:5), intent(in)    :: start0,dtrial0,center
+            real(fpp),                 intent(in)    :: radius,s0
+            real(fpp),                 intent(inout) :: F0,Ftrial
+            real(fpp),                 intent(out)   :: alpha
+            real(fpp), dimension(0:5)                :: start,gradF,dev,dev_temp,temp,dev0
+            real(fpp)                                :: alphanew,err0,err1,beta
+            integer                             :: counter
+            call tensor_deviator(start0,dev0)
+            alpha=F0/(F0-Ftrial)
+            beta=0d0
+            alphanew=0d0
+            start(0:5)=start0(0:5)
+            temp(0:5)=start(0:5)+alpha*dtrial0(0:5)
+            do counter=0,9
+                call tensor_deviator(temp, dev_temp)
+                call tensor_deviator(start,dev)
+                call tau_mises(-dev+dev_temp,err0)
+                call tau_mises(dev-dev0,err1)
+                err0=err0/err1
+                call mises_yld_locus(start,center,radius,s0,F0,gradF)
+                call mises_yld_locus(temp,center,radius,s0,Ftrial,gradF)
+                start(0:5)=temp(0:5)
+                if (abs(Ftrial).le.FTOL .or. err0.lt.0.0000001) exit
+                alphanew=alpha-(Ftrial/(Ftrial-F0))*(alpha-beta)
+                beta=alpha
+                alpha=alphanew
+                temp(0:5)=start(0:5)-(alpha-beta)*dtrial0(0:5)
+
+            enddo
+
+        end subroutine gotoFsec
 end module nonlinear2d
 !! mode: f90
 !! show-trailing-whitespace: t
