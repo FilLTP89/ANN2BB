65ea033be6f2e33f8d73fa99b8392a1222540ee3
Filippo <filippo.gatti@centralesupelec.fr>
2018-04-24 12:17:23 +0200

    small modif

--- syn2ann/src/super_fft.m
+++ syn2ann/src/super_fft.m
@@ -17,8 +17,8 @@
 % * _vfr (frequency column vector)_
 % * _amp (FS amplitude column vector)_
 % * _ang (FS phase column vector)_
-% * _fsr_ (Fourier Complex Spectrum)
-% * _psd (Power Spectral Density column vector)_
+% * _fsr (Fourier Complex Spectrum)_
+% * _psd  (Power Spectral Density column vector)_
 % * _nTm (Main Natural Period evaluated between fcut_low and fcut_high)_
 %% N.B.:
 % nfr should be odd number to be able to transpose the
