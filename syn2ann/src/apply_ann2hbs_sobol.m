c6c92d3954b98b1d6f9e0c1866a3d1c90784ca5e
Filippo Gatti - vigne <filippo.gatti@centralesupelec.fr>
2018-10-25 17:27:28 +0200

    Test ANN2BB sensitivity with soboli

---
+++ syn2ann/src/apply_ann2hbs_sobol.m
@@ -0,0 +1,21 @@
+%% *Train ANN network onto synthetics*
+% _Editor: Filippo Gatti
+% CentraleSupélec - Laboratoire MSSMat
+% DICA - Politecnico di Milano
+% Copyright 2016_
+%% NOTES
+% _apply_ann2hbs_justPSA_: function to apply trained ANN onto hybrid LF/HF
+% synthetics, upon PSA(LF) values
+%% INPUT:
+% * _hbs (hybrid synthetics structure)_
+% * _ann (trained Artificial Neural Network (ANN) structure)_
+%% OUTPUT:
+% * _trs (trained/simulated ann structure)_
+%% *N.B.*
+% Need for:
+% _sim.m,trann_check_vTn.m_
+
+function [out] = apply_ann2hbs_sobol(x)
+    global no contr epsilon1 
+        out = 10.^(sim(ann.(hbs.mon.cp{j_}).net,inp(i_,:)'));
+        out = out./100;
--- syn2ann/src/test_ann_sobol.m
+++ syn2ann/src/test_ann_sobol.m
@@ -6,6 +6,5 @@ trann_setup_sensitivity_sobol;
 syn2ann_pbs_drive;
 
 syn2ann_ann_drive_sobol;
-keyboard
-trs.sps = apply_ann2hbs_justPSA(pbs.org,ann);
-%[S,ST,retourTout] = Sobol(2,1,0,0,4,1000,VVarEntree,'trann_ann_sensitivity_sobol(x)',2);
+%trs.sps = apply_ann2hbs_justPSA(pbs.org,ann);
+[S,ST,retourTout] = Sobol(2,1,0,0,4,1000,VVarEntree,'apply_ann2hbs_sobol(x)',2);
