192756602e84f68026637abae9fcd2f11660c989
Filippo Gatti - vigne <filippo.gatti@centralesupelec.fr>
2018-10-29 19:06:06 +0100

    [BUGFIX] ANN2BB sobol sensitivity plot

--- syn2ann/src/test_ann_sobol.m
+++ syn2ann/src/test_ann_sobol.m
@@ -7,11 +7,15 @@ syn2ann_pbs_drive;
 
 syn2ann_ann_drive_sobol;
 %trs.sps = apply_ann2hbs_justPSA(pbs.org,ann);
-dsx = 1;
-srt = numel(trs.sps.(hbs.mon.cp{j_}).tid);
+dssx = 1:2;
+srt = numel(trs.sps.(hbs.mon.cp{1}).tid);
 hbs.clc = 1000;
-[S,ST,retourTout] = Sobol(2,1,0,0,size(VVarEntree{1,1},1),hbs.clc,VVarEntree{1,1},'apply_ann2hbs_sobol(x)',2);
-
-test_ann_sobol_plot;
-
-keyboard
+for j_=1:numel(dssx)
+    dsx = dssx(j_);
+    for i_=1:hbs.mon.na
+        [S,ST,retourTout] = Sobol(2,1,0,0,size(VVarEntree{i_,j_},1),hbs.clc,...
+            VVarEntree{i_,j_},'apply_ann2hbs_sobol(x)',2);
+        save(sprintf('results_sobol_ann2bb_%u_%u.mat',i_,j_),'hbs','trs','S');
+        test_ann_sobol_plot;
+    end
+end
--- syn2ann/src/test_ann_sobol_plot.m
+++ syn2ann/src/test_ann_sobol_plot.m
@@ -1,21 +1,23 @@
 plot_set_up;
+close all;
 %hfg=figure('position',[0,0,12,12]);
 xpl = cell(2*srt,1);
 ypl = cell(2*srt,1);
 leg = cell(srt,1);
-col = jet(srt);
+col = parula(srt);
 
-for i_=1:srt
-    xpl{2*i_-1,1} = hbs.mon.vTn(trs.sps.(hbs.mon.cp{dsx}).iid);
-    ypl{2*i_-1,1} = S{i_,1}(:,2);
-    xpl{2*i_-0,1} = hbs.mon.vTn(trs.sps.(hbs.mon.cp{dsx}).iid);
-    ypl{2*i_-0,1} = S{i_,1}(:,3);
-    leg{i_,1} = sprintf('$Sa[Tn = %.1f]$',hbs.mon.vTn(trs.sps.(hbs.mon.cp{dsx}).tid(i_)));
+for k_=1:srt
+    xpl{2*k_-1,1} = hbs.mon.vTn(trs.sps.(hbs.mon.cp{dsx}).iid);
+    ypl{2*k_-1,1} = S{k_,1}(:,2);
+    xpl{2*k_-0,1} = hbs.mon.vTn(trs.sps.(hbs.mon.cp{dsx}).iid);
+    ypl{2*k_-0,1} = S{k_,1}(:,3);
+    ypl{2*k_-0,1}(ypl{2*k_-0,1}<=0) = 0.0001;
+    leg{k_,1} = sprintf('$Sa[Tn = %.1f]$',hbs.mon.vTn(trs.sps.(hbs.mon.cp{dsx}).tid(k_)));
 end
-fpplot('xpl',xpl,'ypl',ypl,'pfg',[0 0 12 12],...
-       'xlb',{'T [s]'},'xlm',{[xpl{1,1}(1),xpl{1,1}(end)]},'xtk',xpl(1,1),...
-       'ylb',{'[1]'  },'ylm',{[-.05,1.0]                 },'ytk',{0:.25:1},...
-       'leg',{leg},'tit',{'Sobol Indices-ANN2BB'});
-set(gca,'TickLabelInterpreter', 'latex');
-saveas(hfg,'trial_sobol.eps','epsc');:xa
-
+[hfg,hax,hpl]=fpplot('xpl',xpl,'ypl',ypl,'pfg',[0 0 12 12],'scl',{'lin'},...
+       'xlb',{'T [s]'},'xlm',{[xpl{1,1}(1),xpl{1,1}(end)]},'xtk',{[xpl{1,1}(1):.5:xpl{1,1}(end)]},...
+       'ylb',{'[1]'  },'ylm',{[-.01,1.0]                 },'ytk',{0:.25:1},...
+       'lwd',1,'lst',{'--'},'mrk',{'o'},'leg',{leg},'tit',{'Sobol Indices-ANN2BB'});
+set(hax,'TickLabelInterpreter', 'latex');
+saveas(hfg,sprintf('results_sobol_ann2bb_%u_%u.eps',i_,j_),'epsc');
+close all;
